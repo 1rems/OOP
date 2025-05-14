b3e9f8297928d6d33e7f9eb6659db130c086d633
İrem Su Tepedelen <iremssutepedelen@gmail.com>
2025-05-14 11:07:07 +0300

    ilk arayüz

--- src/Proje/Main.java
+++ src/Proje/Main.java
@@ -1,19 +1,33 @@
 package Proje;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+	
+	@Override
 	public void start(Stage primaryStage) {
-		Label label= new Label("Merhaba javaFX!");
-		StackPane root = new StackPane(label);
-		Scene scene = new Scene(root,300,200);
+		//başlık
+		Label label = new Label("Görev Listesi");
 		
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("First");
-		primaryStage.show();
+		//buton
+		Button button = new Button("Görev Ekle");
+		
+		//dikey düzen
+		VBox vBox= new VBox(10);
+		vBox.getChildren().addAll(label, button);
+		
+        // Sahne 
+        Scene scene = new Scene(vBox, 300, 200);
+
+        // Pencere ayarları
+        primaryStage.setTitle("Görev Takip Uygulaması");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
