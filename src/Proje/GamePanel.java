dc08c446cdbcba2f4d1176b79bf6b2401de000b0
aysegul08 <aysglpnrylmz@gmail.com>
2025-05-22 12:13:32 +0300

    Comment eklendi

--- src/Proje/GamePanel.java
+++ src/Proje/GamePanel.java
@@ -1,8 +1,6 @@
 package Proje;
 
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -15,10 +13,9 @@ public class GamePanel extends AbstractMapPanel {
 	@Override
 	public void drawMap() {
 		this.getChildren().clear();
-		
-		int currentLevel = user.getLevel();
 
 	    for (int i = 1; i <= 10; i++) {
+	    	
 	        Circle node = new Circle(25);
 	        node.setFill(i <= user.getLevel() ? Color.LIGHTCORAL : Color.LIGHTPINK);
 
@@ -26,29 +23,12 @@ public class GamePanel extends AbstractMapPanel {
 	        number.setTextFill(Color.WHITE);
 	        number.setStyle("-fx-font-weight: bold;");
 
-	        StackPane circleWithNumber = new StackPane(node, number);
-	        double x = i * 70;
-	        double y = 65;
-	        
-	        circleWithNumber.setLayoutX(x);
-	        circleWithNumber.setLayoutY(y);
+	        StackPane circleWithNumber = new StackPane();
+	        circleWithNumber.getChildren().addAll(node, number);
+	        circleWithNumber.setLayoutX(i * 70);
+	        circleWithNumber.setLayoutY(65);
 
 	        this.getChildren().add(circleWithNumber);
-	        
-	        if (i == currentLevel) {
-	            try {
-	                Image characterImage = new Image(getClass().getResource("/images/catt.png").toExternalForm());
-	                ImageView characterView = new ImageView(characterImage);
-	                characterView.setFitWidth(70); // boyut ayarı
-	                characterView.setFitHeight(40);
-	                characterView.setLayoutX(x - 15); // ortalamak için ayar
-	                characterView.setLayoutY(y - 33); // dairenin üstüne koy
-
-	                this.getChildren().add(characterView);
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
 	    }
 	}
 		
